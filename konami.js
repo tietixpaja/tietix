0676957ffc40acba79cd0e5ec66813dde56a86bd
Rantala <tietixpaja@gmail.com>
2019-06-20 10:29:17 +0300

    Rantala: Css korjaus

--- konami.js
+++ konami.js
@@ -1,6 +1,6 @@
 let cursor = 0;
-const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
+const KONAMI_CODE = [38, 40, 37, 39, 66];
+/*const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];*/
 document.addEventListener('keydown', (e) => {
   cursor = (e.keyCode == KONAMI_CODE[cursor]) ? cursor + 1 : 0;
   if (cursor == KONAMI_CODE.length) activate();
